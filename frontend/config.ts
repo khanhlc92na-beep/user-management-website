e4de68229e90ce8c58204f7c4229b891929b88ec
Leap <git@leap.new>
2025-08-31 04:33:15 +0000

    Manual code change

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,6 +1,6 @@
 // Google OAuth Client ID for authentication
 // TODO: Set this to your Google OAuth Client ID from Google Cloud Console
-export const googleClientId = "";
+export const googleClientId = "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";
 
 // API base URL - automatically configured for Encore.ts
-export const apiBaseUrl = "";
+export const apiBaseUrl = "https://user-management-website-d2psr4k82vjt2bj6uubg.lp.dev";
